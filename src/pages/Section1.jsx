e0c6133de32482e9dfe2df3b77f2ca0bb0e99024
HenujaKa <katuwapitiyah@gmail.com>
2024-12-16 13:19:06 +0530

    State management
    using React.usestate and props working with child components

---
+++ src/pages/Section1.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+
+function Section1(props) {
+
+    const ingredientList = props.ingeredient.map((item)=>{
+        return <li>{item}</li>
+      })
+  return (
+    <div>
+      <section>
+          <h2>Ingredients on hand:</h2><br /> 
+          <ul>
+            {ingredientList}
+          </ul>
+          {props.ingeredient.length>3 &&<div className='get-recipe'>
+            <div>
+              <h3>Ready for recipe</h3>
+              <p>Generate a recipe from your list of ingredients</p>
+            </div>
+            <button onClick={props.toggleRecipe}>Get a recipe</button>
+          </div>}
+        </section>
+    </div>
+  )
+}
+
+export default Section1;
